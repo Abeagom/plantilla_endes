605e707aeef2caab7d717c2526b64998fd2c91ff
Abeagom <beas.gomez.alberto@iescamas.es>
2025-03-20 10:01:55 +0100

    Añadida refactorización

--- plantilla/src/main/java/com/endes/entidad/Plantilla.java
+++ plantilla/src/main/java/com/endes/entidad/Plantilla.java
@@ -27,6 +27,11 @@ public class Plantilla {
         if (empleado == null) {
             throw new IllegalArgumentException("No se puede contratar un empleado nulo");
         }
+        for(Empleado e : empleados) {
+        	if(e.getDni().equals(empleado.getDni())) {
+                throw new IllegalArgumentException("El empleado con DNI " + empleado.getDni() +" ya está contratado");
+        	}
+        }
         empleados.add(empleado);
     }
 
